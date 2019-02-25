fdda52e5e90b28df9f2f69bc9432ba4193eafad3
Derek Stobbs <dstobbs@googlemail.com>
2019-02-25 10:16:08 +0000

    Calculator: Basic Operators
    - Created the calculator class, with static methods for * / + -

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculator
+{
+	public class Calculator
+	{
+		public static double Multiply(double num1, double num2)
+		{
+			return num1 * num2;
+		}
+
+		public static double Divide(double num1, double num2)
+		{
+			return num1 / num2;
+		}
+
+		public static double Add(double num1, double num2)
+		{
+			return num1 + num2;
+		}
+
+		public static double Subtract(double num1, double num2)
+		{
+			return num1 - num2;
+		}
+	}
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var num1 = 1;
+			var num2 = 2;
+
+			Console.WriteLine("{0} * {1} = {2}", num1, num2, Calculator.Multiply(num1, num2));
+			Console.WriteLine("{0} / {1} = {2}", num1, num2, Calculator.Divide(num1, num2));
+			Console.WriteLine("{0} + {1} = {2}", num1, num2, Calculator.Add(num1, num2));
+			Console.WriteLine("{0} - {1} = {2}", num1, num2, Calculator.Subtract(num1, num2));
+		}
+	}
+}
